09ea9bf11f8cc023e8ea64583605c9037bec6347
Gideon Idowu <simplygiddy@gmail.com>
2023-05-30 20:24:24 +0100

    wait groups and go routines

--- main.go
+++ main.go
@@ -2,59 +2,97 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sync"
 	"time"
 )
 
-type Server struct {
-	exitch chan struct{}
-	msgch  chan string
-}
-
-func NewServer() *Server {
-	return &Server{
-		exitch: make(chan struct{}),
-		msgch:  make(chan string, 128),
-	}
-}
-
-func (s *Server) start() {
-	fmt.Println("server starting")
-	s.loop()
-}
-
-func (s *Server) sendMsg(msg string) {
-	s.msgch <- msg
-}
-
-func (s *Server) loop() {
-mainloop:
-	for {
-		select {
-		case <-s.exitch:
-			//do some stuff when we need to quit
-			fmt.Println("quitting server")
-			break mainloop
-		case msg := <-s.msgch:
-			//do some stuff when we have a message
-			s.handleMsg(msg)
-		}
-	}
-	fmt.Println("server shutting down gracefully")
-}
-
-func (s *Server) exit() {
-	close(s.exitch)
-}
-
-func (s *Server) handleMsg(msg string) {
-	fmt.Println("we received a message: ", msg)
+type UserProfile struct {
+	ID       int
+	Comments []string
+	Likes    int
+	Friends  []int
 }
 
 func main() {
-	server := NewServer()
-	go func() {
-		time.Sleep(time.Second * 5)
-		server.exit()
-	}()
-	server.start()
+	start := time.Now()
+	userProfile, err := handleGetUserProfile(11)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("userProfile: ", userProfile)
+	fmt.Println("fetching the user profile took: ", time.Since(start))
+}
+
+type Response struct {
+	data any
+	err  error
+}
+
+func handleGetUserProfile(id int) (*UserProfile, error) {
+	var (
+		resCh = make(chan Response, 3)
+		wg    = sync.WaitGroup{}
+	)
+	// we are making 3 requests inside their own goroutine
+	go getComments(id, resCh, &wg)
+	go getLikes(id, resCh, &wg)
+	go getFriends(id, resCh, &wg)
+
+	//add the number of requests which is 3
+	wg.Add(3)
+
+	//block until our wg 'counter' is 0 and then unblock
+	wg.Wait()
+	close(resCh)
+
+	userProfile := &UserProfile{}
+	for response := range resCh {
+
+		if response.err != nil {
+			return nil, response.err
+		}
+
+		switch msg := response.data.(type) {
+		case int:
+			userProfile.Likes = msg
+		case []int:
+			userProfile.Friends = msg
+		case []string:
+			userProfile.Comments = msg
+		}
+	}
+	return userProfile, nil
+}
+
+func getComments(id int, resCh chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 200)
+	comments := []string{
+		"This looks good",
+		"LGTM!",
+		"Insightful piece!",
+	}
+	resCh <- Response{
+		data: comments,
+		err:  nil,
+	}
+	wg.Done()
+}
+
+func getLikes(id int, resCh chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 200)
+	resCh <- Response{
+		data: 33,
+		err:  nil,
+	}
+	wg.Done()
+}
+
+func getFriends(id int, resCh chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
+	resCh <- Response{
+		data: []int{11, 34, 78, 455},
+		err:  nil,
+	}
+	wg.Done()
 }
